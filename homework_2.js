0bcfc482c2607022bcec0749cd57b54bcb151b0d
KirillRuzaev <93313571+KirillRuzaev@users.noreply.github.com>
2021-11-01 18:57:08 +0300

    Add files via upload

---
+++ homework_2.js
@@ -0,0 +1,17 @@
+"кот" > "код"//true +
+"2" + 2 * "2"//24 - 
+undefined == null//true+
+undefined === nul//false+
+null == 0//true+
+2 > "3"//false+
+null - false + true//1+
+1 / "l"//nan+
+"2" * "3"//6+
+4 + 5 + "O"//90+
+"l" + 4 + 5// 145+
+"4" - 2// 2
+"4" - "4x"//nan
+null == false// true
+" -4 "/ 0 + 1// infinity???
+null + 1//1
+undefined + null//NaN
